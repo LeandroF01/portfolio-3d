12ffec41ef5b9b65d48657c36506f76cf9b296a3
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-13 20:14:52 -0300

    Add computer to home

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ComputersCanvas from "./Computer";
 
 export const Home = () => {
 	return (
 		<section id="">
 			<h1>Leandro Farias</h1>
+			<div className="w-full h-auto">
+				<ComputersCanvas />
+			</div>
 		</section>
 	);
 };
