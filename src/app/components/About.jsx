4721b6f6b8b58e2645c362718ca739fe5b435596
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-14 19:19:59 -0300

    Add content to about me

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const About = () => {
-	return <div>About</div>;
+	return (
+		<section>
+			<h2>About me</h2>
+			<p>
+				+1 year as a Front-End Web Developer. I have strong knowledge of
+				languages such as JavaScript, TypeScript, HTML, CSS, SASS, frameworks
+				like ReactJs and Tailwind, and experience in Back-End development with
+				NodeJs. Committed to the success of the company, I work collaboratively
+				to achieve established goals. I can also provide innovative and
+				high-quality solutions that enhance your online presence.
+			</p>
+		</section>
+	);
 };
