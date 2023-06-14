3505781272d26f3ed21f74a46a56ce9c61ce057f
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-14 19:19:39 -0300

    Refactor scale and position computer

--- src/app/components/Computer.jsx
+++ src/app/components/Computer.jsx
@@ -22,9 +22,9 @@ const Computers = ({ isMobile }) => {
 			<pointLight intensity={1} />
 			<primitive
 				object={computer.scene}
-				scale={isMobile ? 0.7 : 1}
-				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-				rotation={[-0.01, -0.2, -0.1]}
+				scale={isMobile ? 0.7 : 0.7}
+				position={isMobile ? [0, -3, -2.2] : [0, -0.25, -1.5]}
+				rotation={[-0.02, -0.5, -0.2]}
 			/>
 		</mesh>
 	);
