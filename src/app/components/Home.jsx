2d69c7efb06b2ac9c88678602b774d7ca5dad0a8
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-18 13:08:06 -0300

    Refactor animation of the name

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -3,9 +3,13 @@ import ComputersCanvas from "./Computer";
 
 export const Home = () => {
 	const text1 = "Hello, I'm Leandro Farias";
-	const letter1 = text1.split("");
+	const letter1 = text1
+		.split("")
+		.map((letter) => (letter === " " ? "\u00A0" : letter));
 	const text2 = "Front-End Developer";
-	const letter2 = text2.split("");
+	const letter2 = text2
+		.split("")
+		.map((letter) => (letter === " " ? "\u00A0" : letter));
 
 	const delayIncrement = 0.1;
 	const letter2Delay = letter1.length * delayIncrement;
@@ -13,10 +17,12 @@ export const Home = () => {
 		<section
 			id=""
 			className="flex justify-center align-middle flex-col w-full h-screen ">
-			<h1 className="h-4 relative animate-textKA font-poppins dark:text-white text-pro-w-black font-bold md:text-[64px] md:leading-[70px] text-[40px] leading-[46px] tracking-[-0.5%] text-center mt-[50px]">
+			<div className=""></div>
+			<h1 className="gap-8 h-4 relative animate-textKA font-poppins dark:text-white text-pro-w-black font-bold md:text-[64px] md:leading-[70px] text-[40px] leading-[46px] tracking-[-0.5%] text-center mt-[50px]">
 				{letter1.map((letter, index) => (
 					<span
 						key={index}
+						className="inline-block"
 						style={{
 							animationDelay: `${index * delayIncrement}s`,
 						}}>
@@ -32,7 +38,7 @@ export const Home = () => {
 						style={{
 							animationDelay: `${letter2Delay + index * delayIncrement}s`,
 						}}
-						className="bg-gradient-to-r from-blue-400 to-purple-600 text-transparent bg-clip-text">
+						className="bg-gradient-to-r from-blue-400 to-purple-600 text-transparent bg-clip-text inline-block">
 						{letter}
 					</span>
 				))}
