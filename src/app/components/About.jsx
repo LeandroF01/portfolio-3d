38a97688b98b3b28fc617905be9bf61c67a3b9d1
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-18 15:28:09 -0300

    Add spin to about me

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import user from "../../assets/user.svg";
+import animation from "../../assets/animation-circle.svg";
 
 export const About = () => {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const moveDiv = () => {
+		setIsHovered(!isHovered);
+	};
+
 	return (
 		<section className="flex justify-center w-full h-full">
-			<article className="relative">
-				<div className="front">
+			<Image
+				className="absolute hero-circle"
+				src={animation}
+				width={600}
+				height={600}
+			/>
+			<article className="relative" onMouseOver={moveDiv} onMouseOut={moveDiv}>
+				<div className="front relative w-[370px] h-[380px] backdrop-blur-[40px] p-[20px] rounded-tl-none rounded-tr-[30px] rounded-br-[40px] rounded-bl-[40px] z-10">
 					<div className="absolute right-5 hexagon w-9 h-9 shadow-shadow-matriz flex justify-center">
 						<Image
 							src={user}
@@ -15,8 +30,8 @@ export const About = () => {
 							alt="Picture of the author"
 						/>
 					</div>
-					<h2>ABOUT ME</h2>
-					<p>
+					<h2 className="my-4 font-extrabold text-lg">ABOUT ME</h2>
+					<p className="text-text-parraf text-base">
 						+1 year as a Front-End Web Developer. I have strong knowledge of
 						languages such as JavaScript, TypeScript, HTML, CSS, SASS,
 						frameworks like ReactJs and Tailwind, and experience in Back-End
@@ -26,7 +41,10 @@ export const About = () => {
 						online presence.
 					</p>
 				</div>
-				<div className="back"></div>
+				<div
+					className={`back absolute w-[350px] h-[380px] rounded-tl-none rounded-tr-[30px] rounded-br-[40px] rounded-bl-[40px] top-0 skew-y-6 translate-y-6 ${
+						isHovered ? "move" : ""
+					} `}></div>
 			</article>
 		</section>
 	);
