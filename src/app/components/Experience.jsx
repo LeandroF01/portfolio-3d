2093edbc29446b718295ada2f6beb54a999a6e65
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-19 15:57:26 -0300

    Add experience

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
-const Experience = () => {
-	return <div>Experience</div>;
+export const Experience = () => {
+	return (
+		<section className="flex justify-center align-middle w-full">
+			<h2></h2>
+			<div className="border-l-2 border-dashed ">
+				<article className="w-80 h-52 mx-6 my-4 bg-experience rounded-xl relative ">
+					<div className="absolute w-48 h-[1px] top-0 left-[20%] bg-border-gradiant"></div>
+					<div className="bg-hexagon-ex hexagon absolute top-1/3 left-[-50px]"></div>
+					<div className="flex justify-between m-4">
+						<h3 className="text-xl text-white">Academlo</h3>
+						<div className="text-sm text-white">
+							<p>MAY 2023</p>
+							<p className="text-gray-400">Finalizado</p>
+						</div>
+					</div>
+					<p className="text-sm text-gray-400 m-2">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+						perferendis veritatis ipsam tempora recusandae necessitatibus labore
+						qui cumque earum, nisi dolorum sapiente aspernatur quam, cum
+						reprehenderit alias debitis repellendus et.
+					</p>
+					<div className="absolute w-48 h-[1px] bottom-0 left-[20%] bg-border-gradiant"></div>
+				</article>
+				<article className="w-80 h-52 mx-6 my-4 bg-experience rounded-xl relative ">
+					<div className="absolute w-48 h-[1px] top-0 left-[20%] bg-border-gradiant"></div>
+					<div className="bg-hexagon-ex hexagon absolute top-1/3 left-[-50px]"></div>
+					<div className="flex justify-between m-4">
+						<h3 className="text-xl text-white">Academlo</h3>
+						<div className="text-sm text-white">
+							<p>MAY 2023</p>
+							<p className="text-gray-400">Finalizado</p>
+						</div>
+					</div>
+					<p className="text-sm text-gray-400 m-2">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+						perferendis veritatis ipsam tempora recusandae necessitatibus labore
+						qui cumque earum, nisi dolorum sapiente aspernatur quam, cum
+						reprehenderit alias debitis repellendus et.
+					</p>
+					<div className="absolute w-48 h-[1px] bottom-0 left-[20%] bg-border-gradiant"></div>
+				</article>
+			</div>
+		</section>
+	);
 };
-
-export default Experience;
