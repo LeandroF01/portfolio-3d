38321483bddc95b3e09f8f0d8b4408d48aa00381
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-06-26 20:29:00 -0300

    Add animation

--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 
 import { Github } from "../components/icons/Github.jsx";
 import { NetLink } from "../components/icons/NetLink.jsx";
 
 export default function Projects() {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const moveDiv = () => {
+		setIsHovered(!isHovered);
+	};
+
+	const animationTech = () => {
+		const delayIncrement = 0.1;
+		const elemet = [1, 2, 3];
+		if (isHovered) {
+			return (
+				<ul className="flex gap-1 z-10">
+					{elemet.map((el, index) => (
+						<li
+							key={index}
+							style={{
+								animationDelay: `${index * delayIncrement}s`,
+							}}
+							className={`hexagon w-8 h-8 bg-icons animate-tec `}>
+							{el}
+						</li>
+					))}
+				</ul>
+			);
+		}
+	};
 	return (
 		<section className="flex justify-center w-full">
 			<article className="w-80 h-auto bg-red-600 rounded-2xl relative">
 				<div className="w-full h-40 bg-blue-500 rounded-2xl"></div>
-				<div className="absolute bottom-[27px] right-2">
-					<ul className="flex gap-1 z-10">
-						<li className="hexagon w-8 h-8 bg-icons"></li>
-						<li className="hexagon w-8 h-8 bg-icons"></li>
-						<li className="hexagon w-8 h-8 bg-icons"></li>
-					</ul>
-				</div>
-				<section className="flex justify-center flex-col bg-animation top-32 absolute w-full rounded-2xl overflow-hidden">
+				<div className="absolute bottom-1/2 right-1/3 ">{animationTech()}</div>
+				<section
+					onMouseOver={moveDiv}
+					onMouseOut={moveDiv}
+					className="flex justify-center flex-col bg-animation top-32 absolute w-full rounded-2xl overflow-hidden">
 					<h3 className="text-center p-2 font-">Lorem ipsum dolor sit amet</h3>
 					<div className="w-72 mx-auto h-[1px] bg-slate-300">
 						<div className="line-shine"></div>
