845a60d1e8b715c09b430d142a7c6a92ac090227
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-07-04 16:51:39 -0300

    Refactor experiences to JSON format

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -1,48 +1,32 @@
 import React from "react";
+import experiences from "../../json/experiences.json";
 
 export const Experience = () => {
 	return (
 		<section
 			id="Experience"
-			className="flex justify-center align-middle w-full">
-			<h2></h2>
-			<div className="border-l-2 border-dashed ">
-				<article className="w-80 h-52 mx-6 my-4 bg-experience rounded-xl relative ">
-					<div className="absolute w-48 h-[1px] top-0 left-[20%] bg-border-gradiant"></div>
-					<div className="bg-hexagon-ex hexagon absolute top-1/3 left-[-50px]"></div>
-					<div className="flex justify-between m-4">
-						<h3 className="text-xl text-white">Academlo</h3>
-						<div className="text-sm text-white">
-							<p>MAY 2023</p>
-							<p className="text-gray-400">Finalizado</p>
+			className="flex flex-col justify-center items-center w-full h-screen">
+			<h2>Experience</h2>
+			<div className="border-l-2 border-dashed h-2/3">
+				{experiences.map((experience) => (
+					<article
+						key={experience.titulo}
+						className="w-80 h-52 mx-6 my-4 bg-experience rounded-xl relative ">
+						<div className="absolute w-48 h-[1px] top-0 left-[20%] bg-border-gradiant"></div>
+						<div className="w-[50px] h-[55px] hexagon bg-hexagon-ex absolute top-1/3 left-[-50px] "></div>
+						<div className="flex justify-between m-4">
+							<h3 className="text-xl text-white">{experience.titulo}</h3>
+							<div className="text-sm text-white">
+								<p>{experience.fecha}</p>
+								<p className="text-gray-400">{experience.estado}</p>
+							</div>
 						</div>
-					</div>
-					<p className="text-sm text-gray-400 m-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-						perferendis veritatis ipsam tempora recusandae necessitatibus labore
-						qui cumque earum, nisi dolorum sapiente aspernatur quam, cum
-						reprehenderit alias debitis repellendus et.
-					</p>
-					<div className="absolute w-48 h-[1px] bottom-0 left-[20%] bg-border-gradiant"></div>
-				</article>
-				<article className="w-80 h-52 mx-6 my-4 bg-experience rounded-xl relative ">
-					<div className="absolute w-48 h-[1px] top-0 left-[20%] bg-border-gradiant"></div>
-					<div className="bg-hexagon-ex hexagon absolute top-1/3 left-[-50px]"></div>
-					<div className="flex justify-between m-4">
-						<h3 className="text-xl text-white">Academlo</h3>
-						<div className="text-sm text-white">
-							<p>MAY 2023</p>
-							<p className="text-gray-400">Finalizado</p>
-						</div>
-					</div>
-					<p className="text-sm text-gray-400 m-2">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-						perferendis veritatis ipsam tempora recusandae necessitatibus labore
-						qui cumque earum, nisi dolorum sapiente aspernatur quam, cum
-						reprehenderit alias debitis repellendus et.
-					</p>
-					<div className="absolute w-48 h-[1px] bottom-0 left-[20%] bg-border-gradiant"></div>
-				</article>
+						<p className="text-sm text-gray-400 m-2">
+							{experience.descripcion}
+						</p>
+						<div className="absolute w-48 h-[1px] bottom-0 left-[20%] bg-border-gradiant"></div>
+					</article>
+				))}
 			</div>
 		</section>
 	);
