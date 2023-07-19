ccdb72e8991bef6de0f7cdd410eca4da79137a49
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-07-19 16:01:21 -0300

    Add FormSubmit

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -20,27 +20,31 @@ export const Contact = () => {
 				}`}>
 				<form
 					className="flex-col bg-form  w-80 h-auto px-6 py-12 space-y-6 overflow-hidden rounded-tr-[40px] max-md:rounded-[0px] max-md:rounded-bl-[40px]"
-					action="#"
+					action="https://formsubmit.co/leandro.dev.fa@gmail.com"
 					method="POST">
 					<div className="line-shine w-full top-0"></div>
 					<div>
 						<div className="flex items-center justify-between">
-							<label className="block text-sm font-medium leading-6 text-white">
+							<label
+								htmlFor="name"
+								className="block text-sm font-medium leading-6 text-white">
 								Name
 							</label>
 						</div>
 						<div className="mt-2">
 							<input
 								id="Name"
-								name="Name"
-								type="Name"
+								name="name"
+								type="text"
 								required
 								className="block w-full rounded-md border-0 py-1.5  text-slate-800  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-slate-800  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 							/>
 						</div>
 					</div>
 					<div>
-						<label className="block text-sm font-medium leading-6 text-white">
+						<label
+							htmlFor="email"
+							className="block text-sm font-medium leading-6 text-white">
 							Email address
 						</label>
 						<div className="mt-2">
@@ -54,11 +58,14 @@ export const Contact = () => {
 						</div>
 					</div>
 					<div>
-						<label className="block text-sm font-medium leading-6 text-white">
+						<label
+							htmlFor="message"
+							className="block text-sm font-medium leading-6 text-white">
 							Message
 						</label>
 						<div className="mt-2">
 							<textarea
+								name="message"
 								required
 								className="block w-full rounded-md text-slate-800 border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-slate-800 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								style={{ overflow: "auto" }}
@@ -70,7 +77,7 @@ export const Contact = () => {
 						<button
 							type="submit"
 							className="flex w-full justify-center rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-							Sign in
+							Submit
 						</button>
 					</div>
 				</form>
