a8194fceb43f6e6c3fbb6bcb4e4ea6a8d6d15317
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-10-24 19:36:28 -0300

    Add head

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,9 +7,12 @@ import "./globals.css";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<head></head>
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>Portafolio</title>
+			</head>
 			<body className="scroll-smooth bg-[#0D1117]">
-				<div className="noise"></div>
 				<header>
 					<Navbar />
 				</header>
