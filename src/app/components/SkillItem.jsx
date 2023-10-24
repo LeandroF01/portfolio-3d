81e990505dec350b076e54316d036b903d579c7b
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-10-24 19:39:46 -0300

    Skill component Optimization

---
+++ src/app/components/SkillItem.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import skills from "../../json/skills.json";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
+
+export const SkillItem = ({ skill, delay }) => {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const getAnimationClass = (position) => {
+		return isHovered
+			? `absolute ${position}-0 w-20 h-[2px] left-10 transition-all`
+			: `hexagon absolute ${position}-0 w-[5px] h-[5px] left-20 transition-all`;
+	};
+
+	return (
+		<article
+			className={`w-40 h-16 bg-experience relative rounded-md animate-slideBlur opacity-0 my-3`}
+			style={{
+				animationDelay: `${delay}s`,
+			}}
+			onMouseOver={() => setIsHovered(true)}
+			onMouseOut={() => setIsHovered(false)}>
+			<div
+				className={getAnimationClass("top")}
+				style={{ background: skill.color }}
+			/>
+			<div className="flex justify-center items-center gap-2">
+				<Image src={skill.imageURL} width={25} height={25} alt={skill.name} />
+				<h3 className="text-center my-2 text-base">{skill.name}</h3>
+			</div>
+			<div
+				className={getAnimationClass("bottom")}
+				style={{ background: skill.color }}
+			/>
+		</article>
+	);
+};
--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -1,28 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
+import { SkillItem } from "./SkillItem";
 import skills from "../../json/skills.json";
 import useIntersectionObserver from "../hooks/useIntersectionObserver.js";
 
 function Skills() {
-	const [hoveredElements, setHoveredElements] = useState({});
-
-	const toggleHover = (key) => {
-		setHoveredElements((prevHoveredElements) => ({
-			...prevHoveredElements,
-			[key]: !prevHoveredElements[key],
-		}));
-	};
-
-	const getAnimationClass = (key, position) => {
-		const isHovered = hoveredElements[key];
-		return isHovered
-			? `absolute ${position}-0 w-20 h-[2px]  left-10 transition-all`
-			: `hexagon absolute ${position}-0 w-[5px] h-[5px] left-20 transition-all`;
-	};
-
 	const [targetRef, isIntersecting] = useIntersectionObserver({
-		threshold: 0.5, // Define el umbral de intersección (50% visible)
+		threshold: 0.5,
 	});
 
 	const delayIncrement = 0.3;
@@ -34,35 +18,11 @@ function Skills() {
 			className="w-full h-screen flex justify-center items-center  snap-start">
 			<section className="max-w-[640px] flex justify-center items-center flex-wrap">
 				{skills.map((skill, index) => (
-					<article
+					<SkillItem
 						key={skill.name}
-						className={`w-40 h-16 bg-experience relative rounded-md animate-slideBlur opacity-0 my-3 ${
-							isIntersecting ? "visible" : "hidden"
-						} `}
-						style={{
-							animationDelay: `${index * delayIncrement}s`,
-						}}
-						onMouseOver={() => toggleHover(skill.name)}
-						onMouseOut={() => toggleHover(skill.name)}>
-						<div
-							className={getAnimationClass(skill.name, "top")}
-							style={{ background: skill.color }}
-						/>
-						<div className="flex justify-center items-center gap-2">
-							<Image
-								className=""
-								src={skill.imageURL}
-								width={25}
-								height={25}
-								alt={skill.name}
-							/>
-							<h3 className="text-center my-2 text-base">{skill.name}</h3>
-						</div>
-						<div
-							className={getAnimationClass(skill.name, "bottom")}
-							style={{ background: skill.color }}
-						/>
-					</article>
+						skill={skill}
+						delay={index * delayIncrement}
+					/>
 				))}
 			</section>
 		</section>
