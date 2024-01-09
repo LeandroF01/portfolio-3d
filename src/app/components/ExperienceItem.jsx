7efc7fcafa04ede263c9ec3e2564beacbc6f0039
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2024-01-09 17:11:30 -0300

    Add logo in experience

--- src/app/components/ExperienceItem.jsx
+++ src/app/components/ExperienceItem.jsx
@@ -35,7 +35,17 @@ export const ExperienceItem = ({ experience, index, isIntersecting }) => {
 				isIntersecting ? "visible" : "hidden"
 			}`}>
 			<div className="absolute w-48 h-[1px] top-0 left-[50%] -translate-x-1/2 bg-border-gradiant"></div>
-			<div className="w-[50px] h-[55px] hexagon bg-hexagon-ex absolute top-1/3 -left-[35px] max-lg:top-1/4 max-lg:-left-[22px] max-lg:w-[40px] max-lg:h-[45px]"></div>
+			<div className="w-[50px] h-[55px] hexagon bg-hexagon-ex absolute top-1/3 -left-[35px] max-lg:top-1/4 max-lg:-left-[22px] max-lg:w-[40px] max-lg:h-[45px]">
+				<div className="w-full h-full flex justify-center items-center">
+					<Image
+						className="bg-cover"
+						src={experience.logo}
+						width={30}
+						height={30}
+						alt={`${experience.titulo}-logo`}
+					/>
+				</div>
+			</div>
 			<header className="flex flex-col  text-gray-300 mx-5 gap-2">
 				<h2 className="text-xl max-lg:text-lg font-semibold text-white">
 					{experience.titulo}
